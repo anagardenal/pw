aa43881543e636ad484dcdf7694c0942272fc306
ANA BEATRIZ BENITES GARDENAL <anab.gardenal@gmail.com>
2026-03-19 12:54:47 +0000

    Aula_02

---
+++ src/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula_02</title>
+</head>
+<body>
+    <!-- <div class="container">
+        <fieldset>
+            <legend>Dados do formulário</legend>
+            <form method="get">
+                <label for="input_nome">Nome</label>
+                <input type="text" name="txtNome" id="input_nome">
+                <input type="submit" value="Enviar">
+            </form>
+        </fieldset>
+    </div> -->
+    <?php
+    //$nome = @$_GET['txtNome'];
+    //echo '<h1>Seja bem-vindo Sr(a). ' .$nome. '</h1>';
+    ?>
+</body>
+</html>
